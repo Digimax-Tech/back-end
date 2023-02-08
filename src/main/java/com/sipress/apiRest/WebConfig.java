e2c3fd4b1cd04de94c09e5e46d77ade043e57899
moussathioune <moussampthioune@gmail.com>
2023-02-08 07:46:17 -0800

    bilan de surface

--- src/main/java/com/sipress/apiRest/WebConfig.java
+++ src/main/java/com/sipress/apiRest/WebConfig.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(false)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("http://localhost")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(false)
+//                .maxAge(3600);
+//    }
 }
